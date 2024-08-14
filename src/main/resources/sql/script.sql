cbc3558e398f83f6a51655328878ffe352b52777
pozasl <loic.pozas@monlaboweb.com>
2024-08-14 07:51:28 +0000

    [MOD] Table name in lower case as they should be

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -1,6 +1,6 @@
 USE chatop;
 
-CREATE TABLE `USERS` (
+CREATE TABLE `users` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255),
   `name` varchar(255),
@@ -9,7 +9,7 @@ CREATE TABLE `USERS` (
   `updated_at` timestamp
 );
 
-CREATE TABLE `RENTALS` (
+CREATE TABLE `rentals` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `surface` numeric,
@@ -21,7 +21,7 @@ CREATE TABLE `RENTALS` (
   `updated_at` timestamp
 );
 
-CREATE TABLE `MESSAGES` (
+CREATE TABLE `messages` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `rental_id` integer,
   `user_id` integer,
@@ -30,7 +30,7 @@ CREATE TABLE `MESSAGES` (
   `updated_at` timestamp
 );
 
-CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
+CREATE UNIQUE INDEX `users_index` ON `users` (`email`);
 
 -- -----------------------------
 -- Frontend dev shouldn't do SQL
@@ -41,6 +41,6 @@ CREATE UNIQUE INDEX `USERS_index` ON `USERS` (`email`);
 
 -- ALTER TABLE `RENTALS` ADD FOREIGN KEY (`id`) REFERENCES `MESSAGES` (`rental_id`);
 
-ALTER TABLE `RENTALS` ADD FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`);
-ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
-ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`);
+ALTER TABLE `rentals` ADD FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (`rental_id`) REFERENCES `rentals` (`id`);
