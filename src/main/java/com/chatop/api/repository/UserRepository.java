5169d0e59871693ee4c8f47f653bfee121227e12
pozasl <loic.pozas@monlaboweb.com>
2024-08-14 09:04:55 +0000

    [MOD] Switched from CrudRepo to JpaRepo

--- src/main/java/com/chatop/api/repository/UserRepository.java
+++ src/main/java/com/chatop/api/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.chatop.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.chatop.api.model.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
 }
