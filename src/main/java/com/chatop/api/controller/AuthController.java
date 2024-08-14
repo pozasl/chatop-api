32d489b14857c06543b1d797b3dbbb55811dd6e0
pozasl <loic.pozas@monlaboweb.com>
2024-08-14 15:48:01 +0000

    [ADD] Auth Controller

---
+++ src/main/java/com/chatop/api/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.chatop.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chatop.api.model.NewUser;
+import com.chatop.api.model.User;
+import com.chatop.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@ModelAttribute NewUser newUser ) throws Exception {
+        return userService.entityToModel(userService.create(newUser));
+    }
+    
+}
