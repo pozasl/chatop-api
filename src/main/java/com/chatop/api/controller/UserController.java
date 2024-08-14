4ec5b65208677162636e9efd0b25811fe5d48f65
pozasl <loic.pozas@monlaboweb.com>
2024-08-14 10:29:33 +0000

    [MOD] getUserById route

--- src/main/java/com/chatop/api/controller/UserController.java
+++ src/main/java/com/chatop/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.chatop.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
-    public Iterable<UserEntity> getUsers() {
-        return this.userService.getUsers();
+    @GetMapping("/user:id")
+    public UserEntity getUserById(@PathVariable int id) throws Exception {
+        return this.userService.getUserById(id);
     }
 
 }
