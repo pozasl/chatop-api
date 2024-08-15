a8a3075b35726af06f37c78da735eadc1fb32a28
pozasl <loic.pozas@monlaboweb.com>
2024-08-15 12:13:36 +0000

    [MOD] Better Auth DTO

--- src/main/java/com/chatop/api/model/AuthInfo.java
+++ src/main/java/com/chatop/api/model/AuthInfo.java
@@ -1,3 +1,24 @@
 package com.chatop.api.model;
 
-public record AuthInfo(String login, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class AuthInfo {
+  @Email(message = "Not an email address")
+  private String email;
+  @NotBlank(message = "Password is mandatory")
+  private String password;
+
+  public AuthInfo(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+}
