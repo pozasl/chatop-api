df4c566dd03e9404720c860362a718b25098cb28
pozasl <loic.pozas@monlaboweb.com>
2024-08-15 09:43:41 +0000

    [MOD] regiser endpoint data validation

--- src/main/java/com/chatop/api/controller/AuthController.java
+++ src/main/java/com/chatop/api/controller/AuthController.java
@@ -1,25 +1,40 @@
 package com.chatop.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.api.model.AuthInfo;
 import com.chatop.api.model.NewUser;
 import com.chatop.api.model.User;
 import com.chatop.api.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@ModelAttribute NewUser newUser ) throws Exception {
+    public User register(@Valid @RequestBody NewUser newUser ) throws Exception {
+        System.out.println(newUser.toString());
         return userService.entityToModel(userService.createUser(newUser));
     }
+
+    // @PostMapping("/login")
+    // public User login(@ModelAttribute AuthInfo auth ) throws Exception {
+
+    // }
+
+    // @GetMapping("/me")
+    // public User me(@ModelAttribute NewUser newUser ) throws Exception {
+        
+    // }
     
 }
--- src/main/java/com/chatop/api/model/NewUser.java
+++ src/main/java/com/chatop/api/model/NewUser.java
@@ -1,10 +1,25 @@
 package com.chatop.api.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class NewUser {
 
+  @NotBlank(message = "Name is mandatory")
   private String name;
+
+  @Email(message = "Email is mandatory")
   private String email;
+
+  @Size(min = 6, message = "Password must be at least 6 character long")
   private String password;
+
+  NewUser (String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
 
   public String getEmail() {
     return this.email;
