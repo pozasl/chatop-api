8b6cb74d64d43a2411e64ca47d32144b24024d7b
pozasl <loic.pozas@monlaboweb.com>
2024-08-15 14:14:17 +0000

    [MOD] Updated API test script

--- src/main/resources/script/test_api.sh
+++ src/main/resources/script/test_api.sh
@@ -1,2 +1,5 @@
 # Test register endpoint
-curl -v -X POST localhost:8080/api/auth/register -d '{"email": "alice@test.com","name":"alice","password": "pass"}' -H 'Content-Type: application/json'
+curl -v -X POST localhost:8080/api/auth/register -d '{"email": "steve@test.com","name":"steve","password": "pass1234"}' -H 'Content-Type: application/json'
+
+# Test login endpoint
+curl -v -X POST localhost:8080/api/auth/login -d '{"email": "steve@test.com", "password": "pass1234"}' -H 'Content-Type: application/json'
