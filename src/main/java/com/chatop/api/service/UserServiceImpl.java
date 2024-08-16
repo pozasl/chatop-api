9e2e51f8bde529fcdd0621b61098d5d2ec32feec
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 09:02:59 +0000

    [MOD] Password encryption in user registration

--- src/main/java/com/chatop/api/service/UserServiceImpl.java
+++ src/main/java/com/chatop/api/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ package com.chatop.api.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.chatop.api.exception.ResourceNotFoundException;
@@ -18,11 +19,13 @@ public class UserServiceImpl implements UserService{
 
     private UserRepository userRepository;
     private UserMapper userMapper;
+    private BCryptPasswordEncoder encoder;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        encoder = new BCryptPasswordEncoder();
     }
 
     @Override
@@ -34,9 +37,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User createUser(NewUser newUser) throws Exception {
-        UserEntity entity = new UserEntity(newUser.getName(), newUser.getEmail(), newUser.getPassword());
-        // TODO: Hash Password
-        System.out.println(entity.toString());
+        UserEntity entity = new UserEntity(newUser.getName(), newUser.getEmail(), encoder.encode(newUser.getPassword()));
         return userMapper.entityToModel(this.userRepository.save(entity));
     }
     
