439403fe2b0615fd1cc899f4f03a20eb7a839517
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 09:06:04 +0000

    [MOD] Updated sql script with crypted password

--- src/main/resources/script/test_api.sh
+++ src/main/resources/script/test_api.sh
@@ -1,5 +1,4 @@
 # Test register endpoint
-curl -v -X POST localhost:8080/api/auth/register -d '{"email": "steve@test.com","name":"steve","password": "pass1234"}' -H 'Content-Type: application/json'
-
+curl -v -X POST localhost:8080/api/auth/register -d '{"email": "alice@test.com","name":"alice","password": "pass1234"}' -H 'Content-Type: application/json'
 # Test login endpoint
-curl -v -X POST localhost:8080/api/auth/login -d '{"email": "steve@test.com", "password": "pass1234"}' -H 'Content-Type: application/json'
+curl -v -X POST localhost:8080/api/auth/login -d '{"email": "alice@test.com", "password": "pass1234"}' -H 'Content-Type: application/json'
