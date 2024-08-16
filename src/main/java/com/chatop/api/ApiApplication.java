9957e532ab42f513b276552f18d24006ad68491c
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 11:13:34 +0000

    [MOD] Enable Config

--- src/main/java/com/chatop/api/ApiApplication.java
+++ src/main/java/com/chatop/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.chatop.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.chatop.api.configuration.RsaKeyProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
