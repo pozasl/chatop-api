1ef3d7acfc77ebd04d6ee4e06b820810443004dd
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 13:28:47 +0000

    [MOD] Static method could help

--- src/main/java/com/chatop/api/service/JwtServiceImpl.java
+++ src/main/java/com/chatop/api/service/JwtServiceImpl.java
@@ -24,18 +24,22 @@ public class JwtServiceImpl implements JwtService{
 
     @Override
     public String generateToken(Authentication auth) {
-        Instant now = Instant.now();
         String scope = auth.getAuthorities().stream()
             .map(GrantedAuthority::getAuthority)
             .collect(Collectors.joining(" "));
+            return createToken(auth.getName(), scope, this.encoder);
+    }
+
+    public static String createToken(String email, String scope, JwtEncoder encoder) {
+        Instant now = Instant.now();
         JwtClaimsSet claims = JwtClaimsSet.builder()
             .issuer("self")
             .issuedAt(now)
             .expiresAt(now.plus(1,ChronoUnit.HOURS))
-            .subject(auth.getName())
+            .subject(email)
             .claim("scope", scope)
             .build();
-        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 
 }
