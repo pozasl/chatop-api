e0a10e769d3c4f141a3f4de08a82072cb14c8b50
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 13:33:30 +0000

    [REM] Useless AuthService

--- src/main/java/com/chatop/api/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.chatop.api.service;
-
-import com.chatop.api.model.AuthInfo;
-import com.chatop.api.model.User;
-
-public interface AuthService {
-    public User authenticate(AuthInfo auth) throws Exception;
-    public User isAuthenticated();
-}
