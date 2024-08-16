dab3cef7a4d5e458da32df623d9c5624ad919fcc
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 13:33:10 +0000

    [MOD] UserController test  without security filter

--- src/test/java/com/chatop/api/UserControllerIntegrationTest.java
+++ src/test/java/com/chatop/api/UserControllerIntegrationTest.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.CoreMatchers.is;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 public class UserControllerIntegrationTest {
 
     @Autowired
