dc7ea4aeec17e0039712a22dea1a61107e2cd69d
pozasl <loic.pozas@monlaboweb.com>
2024-08-16 13:32:09 +0000

    [MOD] JWT implementation in Auth controller

--- src/main/java/com/chatop/api/controller/AuthController.java
+++ src/main/java/com/chatop/api/controller/AuthController.java
@@ -1,15 +1,25 @@
 package com.chatop.api.controller;
 
+import java.util.Objects;
+
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chatop.api.model.AuthInfo;
+import com.chatop.api.model.JwtInfo;
 import com.chatop.api.model.NewUser;
 import com.chatop.api.model.User;
-import com.chatop.api.service.AuthService;
+import com.chatop.api.service.JwtService;
+import com.chatop.api.service.JwtServiceImpl;
 import com.chatop.api.service.UserService;
 
 import jakarta.validation.Valid;
@@ -18,29 +28,39 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private AuthenticationManager authenticationManager;
     private UserService userService;
-    private AuthService authService;
+    private JwtService jwtService;
 
     @Autowired
-    public AuthController(UserService userService, AuthService authService) {
+    public AuthController(AuthenticationManager authenticationManager, UserService userService, JwtServiceImpl jwtService) {
+        this.authenticationManager = authenticationManager;
         this.userService = userService;
-        this.authService = authService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
-    public User register(@Valid @RequestBody NewUser newUser ) throws Exception {
-        System.out.println(newUser.toString());
-        return userService.createUser(newUser);
+    public JwtInfo register(@Valid @RequestBody NewUser newUser ) throws Exception {
+        userService.createUser(newUser);
+        Authentication authentication = authenticationManager
+            .authenticate(new UsernamePasswordAuthenticationToken(newUser.getEmail(), newUser.getPassword()));
+        String token = jwtService.generateToken(authentication);
+        return new JwtInfo(token);
     }
 
     @PostMapping("/login")
-    public User login(@Valid @RequestBody AuthInfo authInfo) throws Exception {
-        System.out.println(authInfo.toString());
-        return authService.authenticate(authInfo);
+    public JwtInfo login(@Valid @RequestBody AuthInfo authInfo) throws Exception {
+        Authentication authentication = authenticationManager
+            .authenticate(new UsernamePasswordAuthenticationToken(authInfo.getEmail(), authInfo.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtService.generateToken(authentication);
+        return new JwtInfo(token);
     }
 
-    // @GetMapping("/me")
-    //     public User me(@RequestHeader  ) throws Exception {
-    // }
+    @GetMapping("/me")
+    public User me(Authentication auth) throws Exception {
+        if(Objects.isNull(auth)) throw new AuthenticationException("Couldn't authenticate");
+      return userService.getUserByEmail(auth.getName());
+    }
     
 }
