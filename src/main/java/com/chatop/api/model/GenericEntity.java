bc3742f9b7c0cd3c93d94accb3ff904095a58ec5
pozasl <loic.pozas@wyca.fr>
2024-08-19 09:29:08 +0000

    [MOD] imports cleaning

--- src/main/java/com/chatop/api/model/GenericEntity.java
+++ src/main/java/com/chatop/api/model/GenericEntity.java
@@ -6,7 +6,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
