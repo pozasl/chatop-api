f0fe2cc484510f47a1671c9a5fed6ea0f82ba7f0
pozasl <loic.pozas@monlaboweb.com>
2024-09-03 12:43:00 +0000

    [FIX] Missing ResponseMessage parameter

--- src/main/java/com/chatop/api/model/ResponseMessage.java
+++ src/main/java/com/chatop/api/model/ResponseMessage.java
@@ -1,8 +1,11 @@
 package com.chatop.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Response message interface.
  */
 public interface ResponseMessage {
+  @JsonProperty("message")
   String message();
 }
