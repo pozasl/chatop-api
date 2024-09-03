bfab87b73cff56fe7b6476d191d1caed024313cb
pozasl <loic.pozas@monlaboweb.com>
2024-09-03 12:23:21 +0000

    [MOD] Removed useless jakarta constraint

--- src/main/java/com/chatop/api/model/NewRental.java
+++ src/main/java/com/chatop/api/model/NewRental.java
@@ -1,30 +1,14 @@
 package com.chatop.api.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
  * New Rental DTO.
  */
 public record NewRental(
-    @NotBlank(message = "Name is mandatory")
-    @Size(max = 255, message = "{validation.name.size.too_long}")
     String name,
-
-    @NotNull
-    @PositiveOrZero(message = "Surface must be a positive number")
     int surface,
-
-    @NotNull
-    @PositiveOrZero(message = "Price must be a positive number")
     int price,
-
     MultipartFile picture,
-
-    @NotNull(message = "Missing description parameter")
-    @Size(max = 2000, message = "{validation.name.size.too_long}")
     String description
 ) {}
